635a63b80c68ea74eae40fbafe8caf7fe8ae3430
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-31 07:08:52 +0800

    [android] added spacer on FeedScreen

--- android/app/src/main/java/com/ralphmarondev/mewzi/features/feed/presentation/FeedScreen.kt
+++ android/app/src/main/java/com/ralphmarondev/mewzi/features/feed/presentation/FeedScreen.kt
@@ -2,8 +2,10 @@ package com.ralphmarondev.mewzi.features.feed.presentation
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -81,6 +83,9 @@ fun FeedScreen() {
                     modifier = Modifier
                         .fillMaxWidth()
                 )
+            }
+            item {
+                Spacer(modifier = Modifier.height(100.dp))
             }
         }
     }
